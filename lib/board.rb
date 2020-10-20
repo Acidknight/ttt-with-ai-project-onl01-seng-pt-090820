b60a87b43284455fd8a73d18a180bdc2d856c3f2
Anthony Leon <acidknights89@gmail.com>
2020-10-20 04:32:03 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,50 +1,38 @@
-  
 class Board
   attr_accessor :cells
-
-  @cells = []
+  def initialize
+    reset!
+  end
 
   def reset!
-    self.cells = Array.new(9, " ")
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
-
-  def initialize
-    self.cells = Array.new(9, " ")
-  end
-
   def display
-    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
     puts "-----------"
-    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
     puts "-----------"
-    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
   end
-
-  def position(user_input)
-    self.cells[user_input.to_i-1]
+  def position(input)
+    input = input.to_i
+    cells[input - 1]
   end
-
   def full?
-    self.cells.all? {|cell| cell == "X" || cell == "O"}
+    cells.none? {|cell| cell == " " || cell == nil}
   end
-
   def turn_count
-    self.cells.count("X") + self.cells.count("O")
+    cells.count {|cell| cell == "X" || cell == "O"}
   end
-
-  def taken?(position) #check board position
-    self.cells[position.to_i-1] == "X" || #board position -1 because of test using range 1-9 (user input numbers)
-    self.cells[position.to_i-1] == "O"
+  def taken?(input)
+    position(input) == "X" || position(input) == "O"
   end
-
-  def valid_move?(position)
-    !taken?(position) && position.to_i >0 && position.to_i <=9
+  def valid_move?(input)
+    input = input.to_i
+    !(taken?(input)) && input.between?(1, 9)
   end
-
-  def update(position, player)
-      self.cells[position.to_i-1] = player.token
+  def update(input, player)
+    input = input.to_i
+    cells[input -1] = player.token
   end
-
-
-end
 end
